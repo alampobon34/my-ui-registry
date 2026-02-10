58dca952dfd8e55cd35a42952d5a1151cde260bf
Sayed Monshurul Alam <75158575+alampobon34@users.noreply.github.com>
2026-02-10 09:51:40 +0600

    add the theme and core depandacy in the registry

--- src/hooks/use-theme.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeContext } from "@/providers/theme-provider";
-import { useContext } from "react";
-
-// Custom hook to use the context easily
-export const useTheme = () => {
-  const context = useContext(ThemeContext);
-  if (!context) throw new Error("useTheme must be used within a ThemeProvider");
-  return context;
-};
